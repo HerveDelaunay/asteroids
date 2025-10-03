c10d8e4dcd8e828246296eb1318d913139661902
HerveDelaunay <delaunay.herve06@gmail.com>
2025-10-03 23:53:32 +0200

    feat: added asteroid field & shots

--- main.py
+++ main.py
@@ -8,25 +8,54 @@ from constants import (
 )
 import pygame
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
+
+from shot import Shot
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    print(
-        f"Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}"
-    )
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.containers[1].update(dt)
+        asteroid_field.containers.update(dt)
+
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                sys.exit("Game Over!")
+
         screen.fill("black")
-        player.draw(screen)
+
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
-        clock.tick(60)
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
